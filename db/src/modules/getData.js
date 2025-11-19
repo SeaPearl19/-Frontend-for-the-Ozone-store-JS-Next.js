57295a84da03eee0fc8ca1bb6b5f202b47caa1e1
@CodeMargarita <99239197+SeaPearl19@users.noreply.github.com>
2025-11-19 03:43:43 +0300

    Add files via upload

--- db/src/modules/getData.js
+++ db/src/modules/getData.js
@@ -1,5 +1,8 @@
-const getData = () => {
-    return fetch('https://frontend-for-ozone-default-rtdb.firebaseio.com//goods.json')
+const getData = (str) => {
+    return fetch(
+        `https://frontend-for-ozone-default-rtdb.firebaseio.com//goods.json`
+
+    )
         .then((response) => {
             return response.json()
         })
